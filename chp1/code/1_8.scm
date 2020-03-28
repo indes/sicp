7d7417e8091dd7b9a4a26ee3c12491df3a648b29
indes <wl@live.it>
2020-03-28 17:49:43 +0800

    1.8 done

--- chp1/code/1_8.scm
+++ chp1/code/1_8.scm
@@ -1,3 +1,31 @@
-(define (sqr x) (* x x))
+; 定义立方根近似函数
+(define (improve x y)
+    (/ (+ (/ y (* x x)) (* 2 x)) 
+        3)
+    )
 
-(sqr 3)
+
+(define (cube-root x)
+    (cube-root-iter 1.0 x))
+
+(define (cube x)
+    (* x x x))
+
+(define (good-enough? guess x)
+    (< (abs (- (cube guess) x)) 0.001))
+
+(define (cube-root-iter guess x)
+    (if (good-enough? guess x)
+        guess
+        (cube-root-iter (improve guess x) x)
+    )
+)
+
+(define (cube x)
+    (* x x x)
+)
+
+(cube-root (cube 3))
+(cube-root (cube 4))
+(cube-root (cube 25))
+
